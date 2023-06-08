869a487f97625f5d5d2fe1f983283b801e19f5f6
fiki2002 <adepitanoluwatosin202@gmail.com>
2023-06-08 23:18:01 +0100

    feat:

---
+++ movie_hub/lib/views/next_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class NextPage extends StatelessWidget {
+  const NextPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
