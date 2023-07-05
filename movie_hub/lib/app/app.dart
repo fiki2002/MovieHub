8c43aeb7224f774a55a3d9c77d940f2123858798
fiki2002 <adepitanoluwatosin202@gmail.com>
2023-07-05 23:28:06 +0100

    feat:

--- movie_hub/lib/app/app.dart
+++ movie_hub/lib/app/app.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:movie_hub/cores/constants/theme.dart';
 import 'package:movie_hub/cores/navigator/app_router.dart';
 import 'package:movie_hub/cores/navigator/route_generator.dart';
-import 'package:movie_hub/features/onboarding/pages/splash_Page.dart';
+// import 'package:movie_hub/features/onboarding/pages/splash_Page.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'MovieHub',
-      initialRoute: SplashPage.route,
+      // initialRoute: SplashPage.route,
        theme: ThemeClass.lightTheme,
       onGenerateRoute: RouteGenerator.generateRoute,
       navigatorKey: AppRouter.instance.navigatorKey,
--- movie_hub/lib/cores/navigator/route_generator.dart
+++ movie_hub/lib/cores/navigator/route_generator.dart
@@ -4,15 +4,14 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart' show kIsWeb;
 import 'package:flutter/material.dart';
 import 'package:movie_hub/cores/navigator/error_route.dart';
-import 'package:movie_hub/features/onboarding/pages/splash_Page.dart';
 
 class RouteGenerator {
   static Route<dynamic> generateRoute(RouteSettings settings) {
     final Object? args = settings.arguments;
 
     switch (settings.name) {
-      case SplashPage.route:
-        return pageRoute(const SplashPage());
+      // case SplashPage.route:
+      //   return pageRoute(const SplashPage());
       default:
         return errorRoute();
     }
