0ae46032d31a63483a478b795ee6ee82c2fd5568
fiki2002 <adepitanoluwatosin202@gmail.com>
2023-07-06 23:28:26 +0100

    feat:

---
+++ movie_hub/lib/features/splash_page/splash_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SplashPage extends StatelessWidget {
+  const SplashPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
